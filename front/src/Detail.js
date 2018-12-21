a263c9aad350cfcf0bda187457d9b7d6cd70a8f8
Robin Guerre <robin.guerre@yahoo.fr>
2018-12-21 10:40:42 +0100

    conflict

--- front/src/Detail.js
+++ front/src/Detail.js
@@ -1,14 +1,7 @@
 import React from "react";
-import { Chip } from "@material-ui/core";
-import Avatar from "@material-ui/core/Avatar";
 
 const Detail = ({ detailLabel, detailPath }) => (
-  <img src={detailPath} />
-  // <Chip
-  //   clickable
-  //   color="primary"
-  //   avatar={<Avatar alt={detailLabel} src={detailPath} />}
-  // />
+  <img src={detailPath} alt={detailLabel} />
 );
 
 export default Detail;
